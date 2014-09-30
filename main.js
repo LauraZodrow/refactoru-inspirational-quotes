51d20a95c14689741913994012379766c92ba4d4
Laura <laura.zodrow@gmail.com>
2014-09-30 15:18:46 -0600

    next commit

--- main.js
+++ main.js
@@ -29,5 +29,21 @@ $(document).on('ready', function() {
 		$(this).parent('.quoteBox').hide();	
 	});
 
-
+	//take all author names and show in new link
+	$(document).on('click', authorInput, function() {
+		var arrAuthor = [];
+		
+		if(authorInput.text() === authorInput.text()) {
+			arrAuthor.push(authorInput[i]);
+		}
+	})
 });
+
+//all quotes then filter out author name where .text() === . text(). hide rest of quotes show only .text 
+
+//possibly a remove and render feature
+
+//create a for loop for everytime an authors name is entered. 
+//if author name is repeated [i] === [i], push author name into an array. 
+//when click on name/ linke show or hide author grouping array (toggle).
+
